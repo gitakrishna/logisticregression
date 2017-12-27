101c240386191c6c932c85b5ae32f37ff8db3917
gitakrishna <33204984+gitakrishna@users.noreply.github.com>
2017-12-26 20:16:05 -0500

    Update Logistic.py

--- Logistic.py
+++ Logistic.py
@@ -9,7 +9,6 @@ def main():
     global numVectors
     global eta
     eta = 0.00000001
-    f1 = open('heart-train.txt')
     seq1 = f1.read()
     m = readNumInputs(seq1)
     m = m+1 # to take into account x0 = 1
@@ -30,9 +29,6 @@ def main():
     numVectorsTest = readNumVectors(seq2)
     testing = getFeaturesTest(seq2, numVectorsTest)
     predictions = makePredictions(theta, numVectorsTest, testing)
-    accuracy = getAccuracy(predictions, testing, numVectorsTest)
-    print "Accuracy: ", accuracy, "%"
-
 
 
 def makePredictions(theta, numVectorsTest, testing):
@@ -45,16 +41,6 @@ def makePredictions(theta, numVectorsTest, testing):
             predictions.append(0)
     return predictions
 
-def LL(theta):
-    sum = 0.0
-    for i in range(0, numVectors):
-        yi = data[i][m]
-        xi = data[i]
-        firstTerm = yi*math.log(sigmoid(thetaTransposeX(theta, xi)))
-        secondTerm = (1-yi)*math.log(1-sigmoid(thetaTransposeX(theta, xi)))
-        sum +=(firstTerm+secondTerm)
-    return sum
-
 def thetaTransposeX(theta, xi):
     sum = 0.0
     for i in range(0, m):
@@ -64,38 +50,6 @@ def thetaTransposeX(theta, xi):
             sum+=(theta[i]*(xi[i]))
     return sum
 
-def gradAscent():
-    theta = []
-    for j in range(0, m):
-        theta.append(0.0)
-    #print len(theta)
-    numIterations = 10000
-    for i in range(0, numIterations):
-        print "iteration: ", i
-        gradient = []
-        for j in range(0, m):
-            gradient.append(0.0)
-        for x in range(0, numVectors):
-            z = calculateZ(theta, x) # NEW LINE
-            y = deriv(x, z)
-            for j in range(0, m):
-                X = data[x][j]
-                gradient[j]+=(X*y)
-        for j in range(0, m):
-            theta[j]+= eta*gradient[j]
-
-    return theta
-
-def deriv(x, z):
-    y = data[x][m]
-    y-=sigmoid(z)
-    return y
-
-def sigmoid(z):
-    val = 1.0
-    denom = 1.0 + math.exp(-z)
-    val/=denom
-    return val
 
 
 def calculateZ(theta, x):
@@ -148,32 +102,6 @@ def getProbY():
     probY.append(num1 / float(numVectors))
     return probY
 
-def getFeatures(seq1):
-    vectors = []
-    k = 7
-    for j in range(0, numVectors):
-        features = []
-        features.append(1) #to make x0 = 1
-        for i in range(0, numInputs-1+numInputs-1+3):
-            if (seq1[k + i] != "\n")&(seq1[k+i]!=',')&(seq1[k+i]!=" ")&(seq1[k+i]!=":"):
-                features.append(int(seq1[k + i]))
-        k = k + numInputs-1 + numInputs-1 + 3
-        vectors.append(features)
-    return vectors
-
-def getFeaturesTest(seq1, numVectorsTest):
-    vectors = []
-    k = 7
-    for j in range(0, numVectorsTest):
-        features = []
-        features.append(1)
-        for i in range(0, numInputs-1 +numInputs-1 +3):
-            #print (k+i)
-            if (seq1[k + i] != "\n")&(seq1[k+i]!=',')&(seq1[k+i]!=" ")&(seq1[k+i]!=":"):
-                features.append(int(seq1[k + i]))
-        k = k + numInputs-1 + numInputs-1 + 3
-        vectors.append(features)
-    return vectors
 
 
 def __iadd__(self, other):
